16aaa4fdd183a005e69b3f9e5841be59c606a306
billz <bill.world@163.com>
2018-01-06 21:05:52 +0800

    添加单例模式

--- PatternCase/singleton.cpp
+++ PatternCase/singleton.cpp
@@ -1,2 +1,36 @@
 //singleton.cpp
-//单例：
+//单例：保证一个类仅有一个实例，并提供一个访问它的全局访问点。
+
+#include <gtest/gtest.h>
+
+class Singleton
+{
+public:
+	~Singleton() {}
+	
+	static Singleton* GetInstance()
+	{
+		if (instance == NULL)
+		{
+			instance = new Singleton();
+		}
+		return instance;
+	}
+
+private:
+	static Singleton* instance;
+	Singleton() {}
+};
+
+Singleton* Singleton::instance = NULL;
+
+
+TEST(Singleton,Singleton)
+{
+	Singleton* single1 = Singleton::GetInstance();
+	Singleton* single2 = Singleton::GetInstance();
+
+	EXPECT_EQ(single1,single2);
+
+	delete single1;
+}
